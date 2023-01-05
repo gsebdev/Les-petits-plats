9c5661dd9dafa539b1536cbfbd80b2e3c71aae6c
Sebastien Gault <sgault.webdev@gmail.com>
2023-01-05 22:06:34 +0100

    keyword search made with for loop

--- src/abstract/MainSearch.js
+++ src/abstract/MainSearch.js
@@ -2,11 +2,15 @@ const keywordSearch = (list, searchString, propertiesToSearch) => {
         if(searchString === ''){
             return list
         }
-        const search = list.filter(element => {
-            return propertiesToSearch.some(key => {
-                return element[key].toLowerCase().indexOf(searchString.toLowerCase()) !== -1
-            })
-        })
+        let search = []
+        for (let i = 0; i < list.length; i++){
+            for (let y = 0; y < propertiesToSearch.length; y++) {
+                if(list[i][propertiesToSearch[y]].toLowerCase().indexOf(searchString.toLowerCase()) !== -1) {
+                    search.push(list[i])
+                    break
+                }
+            }
+        }
         return search
     }
 

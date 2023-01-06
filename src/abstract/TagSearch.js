803c745e805900202e8ac78266ba0f063d6095c8
Sebastien Gault <sgault.webdev@gmail.com>
2023-01-06 13:17:43 +0100

    tag search with reduce

--- src/abstract/TagSearch.js
+++ src/abstract/TagSearch.js
@@ -1,11 +1,17 @@
 const filterByTags = (list, tagsObject) => {
-        return list.filter(element =>
-            Object.keys(tagsObject).every((key) => 
+        return list.reduce((filteredList, element) => {
+
+        
+            if(Object.keys(tagsObject).every((key) => 
                 tagsObject[key].every(tag => 
                     element.filters[key].toString().toLowerCase().indexOf(tag) !== -1
                 )
-            )
-        )
+            )){
+                filteredList.push(element)
+            }
+            return filteredList
+            }
+        ,[])
 }
 
 export default filterByTags
